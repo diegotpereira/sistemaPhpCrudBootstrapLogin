bcdbd0a9cb868c6f7fdc96f2eb7d0505619143f3
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-01-22 16:54:24 -0300

    alteração do arquivo confi.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-//include_once('include/Database.php');
+include_once('include/Database.php');
 define('SS_DB_NAME', 'db_sistema_crud_php_login');
 define('SS_DB_USER', 'root');
 define('SS_DB_PASSWORD', 'root');
@@ -13,6 +13,6 @@ try {
 	echo "Connection failed: " . $e->getMessage();
 }
 $conexao = mysqli_connect(SS_DB_HOST, SS_DB_USER, SS_DB_PASSWORD, SS_DB_NAME) or die ('Não foi possível conectar');
-//$db 	=	new Database($pdo);
+$db 	=	new Database($pdo);
 ?>
 
